ef6aef4f9de4c69f219742898564e690ce57da7b
arunchauhan <arunchauhan578@gmail.com>
2024-09-20 14:13:10 +0530

    Lifecycle of Spring managed bean demo

---
+++ src/main/java/guru/springframework/sfgdi/services/CustomBeanPostProcessor.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.beans.*;
+import org.springframework.beans.factory.config.*;
+import org.springframework.stereotype.*;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+
+        return bean;
+    }
+}
